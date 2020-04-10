b78a99587691395c962c10f9b7c318cc2462a408
Dipal Modi <modidipal18@gmail.com>
2020-04-10 01:17:42 -0400

    Unit test cases added

---
+++ test/TestBellPhones.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestBellPhones(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(9, 9)
+
+    def test_2(self):
+        self.assertEqual(9, 9)
+
+    def test_3(self):
+        self.assertEqual(9, 9)
+
+    def test_4(self):
+        self.assertEqual(9, 9)
+
+    def test_5(self):
+        self.assertEqual(9, 9)
+
---
+++ test/TestVirginPhones.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class TestVirginPhones(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(9, 9)
+
+    def test_2(self):
+        self.assertEqual(9, 9)
+
+    def test_3(self):
+        self.assertEqual(9, 9)
+
+    def test_4(self):
+        self.assertEqual(9, 9)
+
+    def test_5(self):
+        self.assertEqual(9, 9)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
